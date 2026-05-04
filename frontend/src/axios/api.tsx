b64f04b6f21a527a8678cc05ec725220cb932b22
Loveneykens Philogene <loveneykens@gmail.com>
2026-05-04 14:26:31 -0300

    Foram modificados

--- frontend/src/axios/api.tsx
+++ frontend/src/axios/api.tsx
@@ -1,7 +1,8 @@
 import Axios  from "axios";
+
  const Api=Axios.create({
 
-     baseURL:"https://script.google.com/macros/s/AKfycbwdTY0wAgYkqNJNAzobv_i7Cy4rgh7ESIlq5R825bAU2IcRMIb-N0MaOH9QrZTCe8Im/exec",
+     baseURL:"https://script.google.com/macros/s/AKfycbwIpmGg9_nNpEeaR5gFUemlfghSodLPbEZVh3EWsDNIRiZ_qMSOFMNBZKMQX3CeT0CGqA/exec",
      headers: { "Content-Type":"text/plain" ,
                 //"Access-Control-Allow-Origin": "*",
    },
